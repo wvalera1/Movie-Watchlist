fdb8980ac7a052caf3d2d192796b4df2ea279fd6
wvalera1 <valera_w1@denison.edu>
2025-03-21 21:59:45 -0400

    Update Watchlist.jsx

--- frontend/src/components/Watchlist.jsx
+++ frontend/src/components/Watchlist.jsx
@@ -1,12 +1,32 @@
 import React from 'react'
+import { removeFromWatchlist } from '../api/api'
+import MovieCard from './MovieCard'
 import './Watchlist.css'
 
-const Watchlist = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const Watchlist = ({ watchlist, refreshWatchlist }) => {
+    const handleRemove = async (id) => {
+        await removeFromWatchlist(id);
+        refreshWatchlist(); // Update watchlist instantly
+    }
+
+    return (
+        <div>
+            <h2>My Watchlist</h2>
+            <div>
+                {watchlist.length > 0 ? (
+                    watchlist.map((movie) => (
+                        <MovieCard
+                            key={movie.imdbID}
+                            movie={movie}
+                            onRemove={() => handleRemove(movie.imdbID)}
+                        />
+                    ))
+                ) : ( 
+                    <p>No movies in your watchlist yet.</p>
+                )}
+            </div>
+        </div>
+    )
 }
 
 export default Watchlist
