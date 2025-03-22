a6215d0ed161227b37cd64d0950ef72eae08805d
wvalera1 <valera_w1@denison.edu>
2025-03-21 21:21:32 -0400

    Update MovieCard.jsx

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import './MovieCard.css'
 
-const MovieCard = () => {
+const MovieCard = ({ movie, onAdd, onRemove}) => {
   return (
     <div>
-      
+        <h3>{movie.Title} ({movie.Year})</h3>
+        <img src={movie.Poster} alt={movie.Title} />
+        {onAdd && <button onClick={() => onAdd(movie)}> +Add to Watchlist</button>}
+        {onRemove && <button onClick={() => onRemove(movie.imdbID)}> Remove </button>}
     </div>
   )
 }
